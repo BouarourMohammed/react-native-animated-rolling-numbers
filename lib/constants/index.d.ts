1f1c0f2d048da561527588cda36ff8afd4d602dc
Jimmy Z <jimmy.zhang@iblum.co>
2024-11-14 16:03:13 -0500

    fix: remove accidental inclusion of the lib output directory in git commit

--- lib/constants/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const NUMBER_ARRAY: any[];
-export declare const COMPACT_NOTATIONS: string[];
-export declare const DIGIT_VARIANTS: {
-    [key: string]: "dot" | "comma" | "sign" | "number" | "compact";
-};
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./components/AnimatedDigit";
-export * from "./components/AnimatedRollingNumber";
-import { AnimatedRollingNumber } from "./components/AnimatedRollingNumber";
-export default AnimatedRollingNumber;
