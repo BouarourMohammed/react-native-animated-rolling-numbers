32d1db4a311334a2da226d766c3422e426370c75
BouarourMohammed <bouarourmohammed93@gmail.com>
2024-11-20 11:46:51 +0100

    Optimize padding calculation for compact notation

--- src/components/AnimatedDigit.tsx
+++ src/components/AnimatedDigit.tsx
@@ -132,6 +132,14 @@ export const AnimatedDigit: React.FC<AnimatedDigitProps> = ({
     [signTextProps, dotTextProps, commaTextProps]
   );
 
+  const resolvedPadding = useMemo(
+    () =>
+      (StyleSheet.flatten(compactNotationStyle)?.fontSize ||
+        StyleSheet.flatten(textStyle)?.fontSize ||
+        14) * 0.1,
+    [textStyle, compactNotationStyle]
+  );
+
   useEffect(() => {
     const newValue = !isNumeric
       ? COMPACT_NOTATIONS.includes(value.toUpperCase())
@@ -180,7 +188,7 @@ export const AnimatedDigit: React.FC<AnimatedDigitProps> = ({
                   textStyle,
                   compactNotationStyle,
                   {
-                    paddingHorizontal: index === 0 ? 1.5 : 0,
+                    paddingHorizontal: index === 0 ? resolvedPadding : 0,
                     position: index === 0 ? "relative" : "absolute",
                     transform: [{ translateY: height * index }],
                   },
